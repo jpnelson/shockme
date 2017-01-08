921642934487c887d4a5bb82eb6aa2066c825696
Joshua Nelson <cyberdash@gmail.com>
2017-01-08 12:46:24 +1100

    serve react app

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+app.use(express.static(__dirname + 'build'));
 
 const port = process.env.PORT || 8080;
 
